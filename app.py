f267eade49a23fe56043a5b5c78bd8054ce154cc
JasonShermanII <ljslayer57@gmail.com>
2025-05-07 17:30:26 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,16 @@ from flask import Flask, request
 import torch
 from PIL import Image
 import io
+import os
 
 app = Flask(__name__)
-model = torch.hub.load('ultralytics/yolov5', 'yolov5s')  # lightweight model
+
+# Cache model and avoid re-downloading on every deploy
+if not os.path.exists('yolov5s.pt'):
+    model = torch.hub.load('ultralytics/yolov5', 'yolov5s')  # lightweight model
+    model.save('yolov5s.pt')  # save model to disk
+else:
+    model = torch.hub.load('ultralytics/yolov5', 'yolov5s', model='yolov5s.pt')  # load from disk
 
 @app.route('/upload', methods=['POST'])
 def upload():
@@ -14,4 +21,5 @@ def upload():
     return results.pandas().xyxy[0].to_json()  # send prediction as JSON
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))  # Use dynamic port from Railway
+    app.run(host='0.0.0.0', port=port)
